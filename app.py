fce3852a7c3b76065b5a8cd5630940e987b8179e
onlysigma <hassuio404@gmail.com>
2025-06-02 12:19:39 +0500

    Update app.py

--- app.py
+++ app.py
@@ -24,4 +24,7 @@ def remove_bg():
     return send_file(io.BytesIO(output_bytes), mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
